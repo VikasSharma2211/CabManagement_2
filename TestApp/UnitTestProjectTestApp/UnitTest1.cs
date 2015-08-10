4be0c4b5e3692b3043655ea30568e026b0bd2487
VikasSharma2211 <vikas_sharma34@infosys.com>
2015-08-10 22:56:33 +0530

    TESTCAB-3

---
+++ TestApp/UnitTestProjectTestApp/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProjectTestApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            MinusLib.Minus m1 = new MinusLib.Minus();
+            Assert.AreEqual(1.0,m1.Subtract(4,1),"It means Minus is not working as expected");            
+
+        }
+    }
+}
